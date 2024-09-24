09ee48fc177bd0a49ce956eb8849cf0cdd5cc433
Emiliano <emikinderk@gmail.com>
2024-09-25 01:15:54 +0200

    Refactor cube animation and camera position

--- main.js
+++ main.js
@@ -1,80 +1,84 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+// Create a scene
 const scene = new THREE.Scene();
 
-const palo = new THREE.BoxGeometry(1, 6, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-const paloMesh = new THREE.Mesh(palo, cubeMaterial);
-paloMesh.position.set(-5, 0, 0);
+// Create a cube
+const cube = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: "grey",
+  wireframe: true,
+});
+const cubeMesh = new THREE.Mesh(cube, cubeMaterial);
+cubeMesh.position.set(0, 1, 0);
+const cubeMesh2 = new THREE.Mesh(cube, cubeMaterial);
+cubeMesh2.position.set(2, 0, 0);
 
-const cubo = new THREE.BoxGeometry(3, 1, 1);
-const cuboMesh = new THREE.Mesh(cubo, cubeMaterial);
-cuboMesh.position.set(-3, 2.5, 0);
+scene.add(cubeMesh);
+scene.add(cubeMesh2);
 
-const cuboMesh2 = new THREE.Mesh(cubo, cubeMaterial);
-cuboMesh2.position.set(-3, 0, 0);
+// cubeMesh.rotation.reorder("YXZ");
+// cubeMesh.rotation.y = THREE.MathUtils.degToRad(90);
+// cubeMesh.rotation.x = THREE.MathUtils.degToRad(45);
 
-const cuboMesh3 = new THREE.Mesh(cubo, cubeMaterial);
-cuboMesh3.position.set(-3, -2.5, 0);
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
-scene.add(paloMesh);
-scene.add(cuboMesh);
-scene.add(cuboMesh2);
-scene.add(cuboMesh3);
-
-const paloMesh2 = new THREE.Mesh(palo, cubeMaterial);
-paloMesh2.position.set(0, 0, 0);
-scene.add(paloMesh2);
-const paloMesh3 = new THREE.Mesh(palo, cubeMaterial);
-paloMesh3.position.set(4, 0, 0);
-scene.add(paloMesh3);
-
-const largo = new THREE.Mesh(new THREE.BoxGeometry(1, 3, 1), cubeMaterial);
-largo.position.set(1, 1.5, 0);
-largo.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 4.9);
-scene.add(largo);
-
-const largo2 = new THREE.Mesh(new THREE.BoxGeometry(1, 3, 1), cubeMaterial);
-largo2.position.set(3, 1.5, 0);
-largo2.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / -4.9);
-scene.add(largo2);
-
-const paloMesh4 = new THREE.Mesh(palo, cubeMaterial);
-paloMesh4.position.set(6, 0, 0);
-scene.add(paloMesh4);
-
-const largo3 = new THREE.Mesh(new THREE.BoxGeometry(1, 3, 1), cubeMaterial);
-largo3.position.set(7, -1.5, 0);
-largo3.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 5);
-scene.add(largo3);
-
-const largo4 = new THREE.Mesh(new THREE.BoxGeometry(1, 3, 1), cubeMaterial);
-largo4.position.set(7, 1.5, 0);
-largo4.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / -4.9);
-scene.add(largo4);
-
-
-
+// Create a Perspective Camera
 const camera = new THREE.PerspectiveCamera(
   30,
   window.innerWidth / window.innerHeight,
   0.1,
   100
 );
+camera.position.set(10, 4, 10);
 
-camera.position.z = 27;
+// Create an Orthographic Camera
+// const aspectRatio = window.innerWidth / window.innerHeight;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   200
+// );
 
+// Inizilize the renderer
 const canvas = document.querySelector("canvas.threejs");
-
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+// Add orbit controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.autoRotate = true;
+// controls.autoRotate = true;
 
+// Resize the renderer when the window is resized
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+// Initialize a clock
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+// Animate the cube
 const animate = () => {
+  const currentTime = clock.getElapsedTime();
+  const delta = currentTime - previousTime;
+
+  previousTime = currentTime;
+
+  cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * delta * 10;
+  cubeMesh2.rotation.x += THREE.MathUtils.degToRad(1) * delta * 10;
+  cubeMesh2.position.y = Math.sin(currentTime) * 0.5;
+
+  cubeMesh.scale.x = Math.sin(currentTime) * 0.5 + 2;
+
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
